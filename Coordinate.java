aeef2f8f20b81a5b69ff84fa62a97560f6d9c6e3
kundan kumar <iaskundan8092@gmail.com>
2025-03-21 23:35:36 +0530

    Update Coordinate.java

--- Coordinate.java
+++ Coordinate.java
@@ -1,38 +1,38 @@
 class Coordinate {
     private int x, y;
 
-    // Method to set coordinate values
+    
     public void setValues(int x, int y) {
         this.x = x;
         this.y = y;
     } 
 
-    // Method to display coordinate values
+    
     public void display() {
         System.out.println("X: " + x + ", Y: " + y);
     }
 
-    // Method to check equality of two coordinates
+    
     public boolean isEqual(Coordinate other) {
         return this.x == other.x && this.y == other.y;
     }
 
     public static void main(String[] args) {
-        // Creating objects using reference variables
+        
         Coordinate number1 = new Coordinate();
         Coordinate number2 = new Coordinate();
 
-        // Assigning values to objects
+        
         number1.setValues(10, 20);
         number2.setValues(10, 20);
 
-        // Displaying the coordinate values
+        
         System.out.println("Number 1:");
         number1.display();
         System.out.println("Number 2:");
         number2.display();
 
-        // Comparing the two coordinates
+       
         if (number1.isEqual(number2)) {
             System.out.println("Both coordinates are equal.");
         } else {
